49018df9eba203c842191f5336ae08004b846334
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-08-04 22:41:23 +0530

    spring boot project

--- src/main/java/com/www/ihub/controller/StudentController.java
+++ src/main/java/com/www/ihub/controller/StudentController.java
@@ -19,7 +19,7 @@ import com.www.ihub.token.StudentToken;
 
 @RestController
 @RequestMapping("/stu")
-@CrossOrigin(origins = "https://student-lc9f.onrender.com")
+@CrossOrigin(origins = "https://student-ohvy.onrender.com/")
 public class StudentController 
 {
 	@Autowired
