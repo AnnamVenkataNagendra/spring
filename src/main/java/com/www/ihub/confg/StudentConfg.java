5aa589c66742a08686beb273c633160f296e297a
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-08-04 22:58:01 +0530

    spring boot project

--- src/main/java/com/www/ihub/confg/StudentConfg.java
+++ src/main/java/com/www/ihub/confg/StudentConfg.java
@@ -7,12 +7,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.www.ihub.service.StudentService;
 
 @Configuration
+@EnableWebSecurity
 public class StudentConfg 
 {
 	
