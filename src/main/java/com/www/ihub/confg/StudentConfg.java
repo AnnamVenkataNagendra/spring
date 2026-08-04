c946945d71fcf982aec12c8520904539728f89e3
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-08-05 01:42:37 +0530

    Configure CORS

--- src/main/java/com/www/ihub/confg/StudentConfg.java
+++ src/main/java/com/www/ihub/confg/StudentConfg.java
@@ -1,20 +1,14 @@
 package com.www.ihub.confg;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 import com.www.ihub.service.StudentService;
@@ -39,7 +33,6 @@ public class StudentConfg
          .csrf(csrf -> csrf.disable())
          
          .authorizeHttpRequests(auth -> auth
-        		 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
              .requestMatchers(
                  "/stu/login",
                  "/stu/post",
