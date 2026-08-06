89f6d342001c15a4d19ac3ca03c9af5fa530d3fe
AnnamVenkataNagendra <annamvenkatanagendra@gmail.com>
2026-08-07 00:16:10 +0530

    spring boot project

--- src/main/java/com/www/ihub/token/StudentToken.java
+++ src/main/java/com/www/ihub/token/StudentToken.java
@@ -1,8 +1,10 @@
 package com.www.ihub.token;
+import java.util.Base64;
 import java.util.Date;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -13,8 +15,11 @@ import io.jsonwebtoken.security.Keys;
 public class StudentToken {
 
 	
-	private final SecretKey TOKEN=Keys.secretKeyFor(SignatureAlgorithm.HS256);
-	
+	private static final String SECRET =
+            "U3VwZXJTZWNyZXRLZXlGb3JKV1RfMTIzNDU2Nzg5MF9TZWN1cmVLZXk=";
+
+    private final SecretKey secretKey =
+            Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET));
 	
 	public String generateToken(String name) {
 		
@@ -22,8 +27,34 @@ public class StudentToken {
 				 .setSubject(name)
 	                .setIssuedAt(new Date())
 	                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-	                .signWith(TOKEN, SignatureAlgorithm.HS256)
+	                .signWith(secretKey, SignatureAlgorithm.HS256)
 	                .compact();
+	}
+	
+	public String validateUserToken(String name)
+	{
+		
+		return Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(name)
+				.getBody()
+				.getSubject();
+		
+	}
+	
+	public Date userLoginData(String name) {
+		return Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(name)
+				.getBody()
+				.getExpiration();
+	}
+	
+	public boolean extractUserName(UserDetails userDetails, String name) {
+		
+		return userDetails.getUsername().equals(validateUserToken(name)) && userLoginData(name).after(new Date());
 	}
 	
 }
